4f88a76353618fd23eaf3c163b2878d4c00731f1
Nikita M <79057198+fadingautumn@users.noreply.github.com>
2021-12-27 22:56:36 +0300

    Update CurrentAnfisaState.py

--- CurrentAnfisaState.py
+++ CurrentAnfisaState.py
@@ -8,6 +8,23 @@ DATABASE = {
     'Коля': 'Красноярск'
 }
 
+def process_friend (name, query):
+    if name in DATABASE:
+        if query == 'ты где?':
+            return f'{name} в городе {DATABASE[name]}'
+        else:
+            return '<неизвестный запрос>'
+    else:
+        return f'У тебя нет друга по имени {name}'
+    
+    
+def process_query(query):
+    i = query.split(', ')
+    if i[0] == "Анфиса":
+        return process_anfisa(i[1])
+    else:
+        return process_friend(i[0],i[1])
+
 def format_friends_count(friends_count):
     if friends_count == 1:
         return '1 друг'
@@ -20,6 +37,7 @@ def format_friends_count(friends_count):
 def process_anfisa(query):
     if query == 'сколько у меня друзей?':
         count = len(DATABASE)
+        format_friends_count(count)
         return f'У тебя {count} друзей.'
     elif query == 'кто все мои друзья?':
         friends_string = ', '.join(DATABASE)
@@ -33,7 +51,10 @@ def process_anfisa(query):
 
 
 print('Привет, я Анфиса!')
-print(process_anfisa('сколько у меня друзей?'))
-print(process_anfisa('кто все мои друзья?'))
-print(process_anfisa('где все мои друзья?'))
-print(process_anfisa('кто виноват?'))
+print(process_query('Анфиса, сколько у меня друзей?'))
+print(process_query('Анфиса, кто все мои друзья?'))
+print(process_query('Анфиса, где все мои друзья?'))
+print(process_query('Анфиса, кто виноват?'))
+print(process_query('Соня, ты где?'))
+print(process_query('Коля, что делать?'))
+print(process_query('Антон, ты где?')) 
